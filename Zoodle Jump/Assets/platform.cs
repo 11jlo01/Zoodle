a5afc3b1bf3d9d67fc68b22b25bb696dfa753094
Janna Loman <11jlo01@skola.falun.se>
2018-05-28 14:29:38 +0200

    hejs

--- Zoodle Jump/Assets/platform.cs
+++ Zoodle Jump/Assets/platform.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class platform : MonoBehaviour {
 
-    public float jumpForce = 10f;
+    public float jumpForce = 10f; //Hur stor kraften är som ger studsen
 
-	void OnCollisionEnter2D(Collision2D collision)
+	void OnCollisionEnter2D(Collision2D collision) //Ger information om vad ett som händer med då ett objekt krockar med ett annat objekt 
     {
-        if (collision.relativeVelocity.y <= 0f)
+        if (collision.relativeVelocity.y <= 0f) //Collision är en variabel som innehåller information om kollisionen (hastighet, typ av objekt osv)
         {
-            Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
-                if (rb != null)
+            Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>(); //Objekten som kolliderar är av typen Rigidbody2D
+                if (rb != null) //Om objektet av typen Rigidbody2D inte är lika med null
                     {
                           Vector2 velocity = rb.velocity;
                           velocity.y = jumpForce;
-                          rb.velocity = velocity;
+                          rb.velocity = velocity; //Adderar kraft som ger studs uppåt
                     }
         }
 
         
-    }
+    } //Då ett objekt krockar med ett annat så utförs koden inom måsvingarna (funktionen)
 }
