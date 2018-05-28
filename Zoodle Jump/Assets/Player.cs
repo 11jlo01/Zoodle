f5e9b51174a84da021bad7c28651d6db8c5e8f54
Mira Kagre <00mka01@skola.falun.se>
2018-05-28 19:24:29 +0200

    Alla script har nu kommentarer

--- Zoodle Jump/Assets/Player.cs
+++ Zoodle Jump/Assets/Player.cs
@@ -2,28 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))] //
+[RequireComponent(typeof(Rigidbody2D))] //Säkerställer att det alltid finns en Rigidbody2D-komponent kopplad till koden
 public class Player : MonoBehaviour {
 
     public float movementSpeed = 10f; //Gör det möjligt att bestämma/ändra karaktärens hastighet.
 
-    Rigidbody2D rb; //
-    float movement = 0f; //
+    Rigidbody2D rb; //Objektet Rigidbody2D också kallad rb
+
+    float movement = 0f; //Variabeln movement tilldelas värdet 0.
     // Use this for initialization
     void Start () {
-        rb = GetComponent<Rigidbody2D>(); //
+        rb = GetComponent<Rigidbody2D>(); //rb tilldelas komponenten Rigidbody2D
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        movement = Input.GetAxis("Horizontal") * movementSpeed; //
+        movement = Input.GetAxis("Horizontal") * movementSpeed; //Vi vill kunna flytta karaktären horistontalt, allstå i x-led. 
+        //Variabeln movement tilldelas ett värde baserat på hur vi styr den (piltangenterna) och hastigheten den förflyttar sig med.
 	}
 
-    void FixedUpdate()
+    void FixedUpdate() //Rörelser ligger i FixedUpdate
     {
-        Vector2 velocity = rb.velocity; //
-        velocity.x = movement; //
-        rb.velocity = velocity; //
+        Vector2 velocity = rb.velocity; //Hämtar hastigheten, gör det möjligt att bestämma hur snabbt spelkaraktären 
+        //ska röra sig då vänster respektive höger piltangent trycks ned.
+        velocity.x = movement; //Variabeln movement tilldelas samma värde som hastigheten i x-led.
+        rb.velocity = velocity; //Velocity tilldelas samma värde som hastigheten hos våran rigidbody, 
+        //alltså kan samma kodslinga nu användas igen, man kan säga att den blir nollställd. 
+        //Ny kod behövs allstå inte skrivas för varje gång karaktären flyttar sig igen. 
     }
 }
 //Scriptet player justerar spelfigurens hastighet och gör det möjligt att styra karaktären i sidled med piltangenterna.
